bf71a064a817930dd17758511832888d7d25a512
davidbison <davidbisonhubert@gmail.com>
2016-02-01 14:30:28 -0800

    Add random module and game logic

--- rpsls.py
+++ rpsls.py
@@ -1,6 +1,6 @@
 # Rock-paper-scissors-lizard-Spock template
 
-
+import random
 # The key idea of this program is to equate the strings
 # "rock", "paper", "scissors", "lizard", "Spock" to numbers
 # as follows:
@@ -44,24 +44,26 @@ def number_to_name(number):
 
 
 def rpsls(player_choice):
-    # delete the following pass statement and fill in your code below
-    pass
+    print
 
-    # print a blank line to separate consecutive games
+    print "Player chooses " + player_choice
 
-    # print out the message for the player's choice
+    player_number = name_to_number(player_choice)
 
-    # convert the player's choice to player_number using the function name_to_number()
+    comp_number = random.randrange(0, 4)
 
-    # compute random guess for comp_number using random.randrange()
+    comp_choice = number_to_name(comp_number)
 
-    # convert comp_number to comp_choice using the function number_to_name()
+    print "Computer chooses " + comp_choice
 
-    # print out the message for computer's choice
+    throw_outcome = (comp_number - player_number) % 5
 
-    # compute difference of comp_number and player_number modulo five
-
-    # use if/elif/else to determine winner, print winner message
+    if throw_outcome == 0:
+        print "Player and computer tie!"
+    elif throw_outcome <= 2:
+        print "Computer wins!"
+    elif throw_outcome > 2:
+        print "Player wins!"
 
 
 # test your code - THESE CALLS MUST BE PRESENT IN YOUR SUBMITTED CODE
