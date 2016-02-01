db8fa2c3788621b1d1636c73c7266dd4ed3add3c
davidbison <davidbisonhubert@gmail.com>
2016-02-01 14:29:15 -0800

    Add function to convert number to throw

--- rpsls.py
+++ rpsls.py
@@ -29,11 +29,18 @@ def name_to_number(name):
 
 
 def number_to_name(number):
-    # delete the following pass statement and fill in your code below
-    pass
-
-    # convert number to a name using if/elif/else
-    # don't forget to return the result!
+    if number == 0:
+        return "rock"
+    elif number == 1:
+        return "Spock"
+    elif number == 2:
+        return "paper"
+    elif number == 3:
+        return "lizard"
+    elif number == 4:
+        return "scissors"
+    else:
+        print "Invalid number: ", number
 
 
 def rpsls(player_choice):
